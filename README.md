07fc944d30f831b90452d3f1318426196f4720de
Max <maxfrank13@gmail.com>
2022-01-03 14:19:46 -0500

    finished first draft

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # Portfolio-site
-A site detailing my skills and services
+
+## Description
+
+My portfolio site demonstrating my skills and services. Using this as a hub for all of my most notable web development accomplishments, I will be able to show prospective employers and partners my capabilities. I will also continue to use the portfolio site as a place to practice and hone unique development techniques (i.e. interactive elements, animations, color palettes, JavaScript applications, etc).
+
+[![Portfolio video tour](/assets/media/images/Portfolio-home.png)](https://maxfrank13.github.io/Portfolio-site/)
+
+
+## What's happening in the code
+
+1. HTML
+    - Head links (following title):
+        - favicon of a folder/briefcase
+        - font-awesome stylesheet for icons
+        - main stylesheet - I prefer to set defaults within a main CSS file unless there are a lot of defaults to be made, rather than using a reset like normalize.css
+    - Preloader
+        - simple preloader icon used while the rest of the page loads
+    - Header
+        - nav bar that converts to fixed once scrolled past
+        - banner image and introduction
+    - Sections
+        - About with image & contact info
+        - Services detailing skills
+        - Projects showing off notable accomplishments & first app
+            - slider incorporated to reduce clutter
+    - Footer & go-to-top link
+
+2. CSS 
+    - Variables
+        - three colors and their shades; then grey-white-black
+        - two font-families
+        - general variables used throughout
+    - Global styles
+        - re-initialize any undesired defaults
+        - scroll-behavior to smooth for cleaner UI
+    - Main content
+        - formatting and configuration
+        - general styling for entire document
+        - classes & properties initialized for use in JS
+            - hide/show classes
+            - slider-container uses `overflow: hidden` in conjuction with JS
+    - Media queries
+        - @ min-width to accomodate mobile-first
+            - widths: 512px, 768px, 1024px
+    - Animation
+        - set keyframes for "pulse" animation
+
+3. JavaScript
+    - Preloader
+        - add hide class on preloader element once page loads
+    - Toggle links menu
+        - using `.toggle()` to switch add/remove class
+    - Fixed Nav and Go-to-Top scroll
+        - hide/show fixed nav depending on how far the user has scrolled using the `pageYOffset` and `getBoundingClientRect()` methods
+        - hide/show Go-to-Top link using similar technique
+    - Smooth scroll with fixed nav
+        - resetting default behavior of navigation links to scroll to a specific spot based on the state of the nav bar
+        - after a navigation link has been clicked, remove links if in small viewport
+    - Profile pic toggle
+        - use `click` events to toggle hide-img class
+    - Slider
+        - set style of slides so that they are organized left-to-right & CSS hides the overflow
+        - counter variable used in conjunction with prev & next buttons to keep track of slides
+        - carousel function accesses `transform` style of each slide and positions based on counter variable
+            - next/prev buttons are hidden if necessary
+
+## What to work on
+
+The video for the Virtual Kitchen is positioned awkwardly at certain screen widths. The content still plays and is visible but the general presentation is a little clunky. Playing around the the styles on that section would be the next step in cleaning up that area.
+
+As of January 2022, the portfolio is generally lacking in content. Continuing to make web applications will obviously help in that process. However, I would also like to begin contributing to open-source projects. This will further demonstrate my development abilities to potential employers and partners while also showing a willingness to work on larger projects.
--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".links-container");
-const navbar = document.querySelector("nav");
-const topLink = document.querySelector(".top-link");
-const portrait_img = document.querySelector(".fixed-img");
-const portrait_icon = document.querySelector(".portrait-icon");
-const close_icon = document.querySelector(".close");
+// **** Preloader ****
 
-console.log(portrait_img);
+const preloader = document.querySelector(".preloader");
+
+// once the page loads, hide preloader
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
 
 // **** Toggle links menu ****
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
 
 navToggle.addEventListener("click", function () {
     linksContainer.classList.toggle("show-links");
@@ -16,15 +18,22 @@ navToggle.addEventListener("click", function () {
 
 // **** Fixed Nav and Go-to-Top scroll **** 
 
+const navbar = document.querySelector("nav");
+const topLink = document.querySelector(".top-link");
+
 window.addEventListener("scroll", function () {
+    // find how far the page has been scrolled
     const scrollHeight = window.pageYOffset;
+    // find height of nav bar
     const navHeight = navbar.getBoundingClientRect().height;
+    // if we have scrolled farther than the height of the nav bar then toggle the fixed-nav on
     if (scrollHeight > navHeight) {
         navbar.classList.add("fixed-nav");
     } else {
+        // otherwise toggle it off - works when we scroll back up
         navbar.classList.remove("fixed-nav");
     }
-
+    // if the page scrolls to a certain point, show the go-to-top link
     if (scrollHeight > 500) {
         topLink.classList.add("show-link");
     } else {
@@ -35,24 +44,37 @@ window.addEventListener("scroll", function () {
 // **** Smooth scroll with fixed nav ****
 
 const scrollLinks = document.querySelectorAll(".scroll-link");
+
 scrollLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
-        e.preventDefault();
+
         // reset default to navigate to a specific spot
+        e.preventDefault();
+
+        // initialize variables using href of clicked target
         const id = e.currentTarget.getAttribute("href").slice(1);
         const element = document.getElementById(id);
 
+        // find heights of navbar and links container
         const navHeight = navbar.getBoundingClientRect().height;
+        console.log(navHeight);
         const containerHeight = linksContainer.getBoundingClientRect().height;
+        
+        // check if fixed-nav is on, returns true/false
         const fixedNav = navbar.classList.contains("fixed-nav");
+       
+        // position is our specified spot's (distance from the top of it's parent) minus (height of nav bar)
         let position = element.offsetTop - navHeight;
-    
+       
+        // position if there is no fixed-nav before scrolling
         if (!fixedNav) {
             position -= navHeight;
         }
+        // check to see if menu is pulled down and adjust position accordingly
         if (navHeight > 88) {
             position += containerHeight;
         }
+        // scroll to our specified position
         window.scrollTo({
             left: 0,
             top: position,
@@ -63,7 +85,13 @@ scrollLinks.forEach((link) => {
     })
 })
 
-// **** Profile pic hover functionality ****
+// **** Profile pic ****
+
+const portrait_img = document.querySelector(".fixed-img");
+const portrait_icon = document.querySelector(".portrait-icon");
+const close_icon = document.querySelector(".close");
+
+// toggling hide-img class
 
 portrait_icon.addEventListener("click", function () {
     portrait_img.classList.remove("hide-img");
@@ -74,3 +102,60 @@ close_icon.addEventListener("click", function() {
     portrait_img.classList.add("hide-img");
     portrait_icon.classList.remove("hide-img");
 })
+
+// **** Slider ****
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+// counter is used as reference for carousel function
+let counter = 0;
+
+// select all slides and order them horizontally - CSS hides overflow
+slides.forEach(function (slide, index) {
+    slide.style.left = `${index * 100}%`;
+});
+
+// setting button listeners and default style
+
+nextBtn.addEventListener("click", function () {
+    counter++;
+    carousel();
+});
+  
+prevBtn.addEventListener("click", function () {
+    counter--;
+    carousel();
+});
+
+prevBtn.style.visibility = "hidden";
+
+// slide function
+
+function carousel() {
+
+// slider with no loop
+    // hide buttons if user is at either end of slide-container
+    if (counter < slides.length - 1) {
+        nextBtn.style.visibility = "visible";
+        } else {
+        nextBtn.style.visibility = "hidden";
+        }
+        if (counter > 0) {
+        prevBtn.style.visibility = "visible";
+        } else {
+        prevBtn.style.visibility = "hidden";
+    }
+    // adjust which slide is in view using counter variable
+    slides.forEach(function (slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+        // change to loop functionality - also need to remove default styles on prev-btn
+        // if (counter === slides.length) {
+        //   counter = 0;
+        // }
+        // if (counter < 0) {
+        //   counter = slides.length - 1;
+        // }
+}
+
+    
